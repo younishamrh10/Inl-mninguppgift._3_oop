e29a72db51e9f3e4e53c418cb8dc3b5cfd43f163
younishamrh10 <yoha06001@utb.vaxjo.se>
2024-04-05 13:29:51 +0200

    Add project files.

---
+++ Class2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inlämninguppgift._3_oop
+{
+    internal class namn
+    {
+        private string helnamn;
+        private string förnamn;
+        private string efternamn;
+
+        public string Förnamm
+        {
+            get { return förnamn; }
+
+            set { förnamn = value; }
+        }
+
+        public string Efternamn
+        {
+            get { return efternamn; }
+
+            set { efternamn = value; }
+        }
+
+        public void Namn()
+        {
+            helnamn = förnamn + " " + efternamn;
+            Console.WriteLine("Hej " + helnamn);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Inlämninguppgift._3_oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            namn p = new namn();
+            Console.WriteLine("Förnamn?");
+            p.Förnamm = Console.ReadLine();
+            Console.WriteLine("Efternamn?");
+            p.Efternamn = Console.ReadLine();
+            Console.WriteLine("");
+            p.Namn();
+
+            bool myndig = false;
+            Console.WriteLine("");
+            Console.WriteLine("När är du född?");
+            int år = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vilken månd föddes du?");
+            int månad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vilken dag föddes du?");
+            int dag = int.Parse(Console.ReadLine());
+            DateTime nu = DateTime.Today;
+            DateTime årsedan = DateTime.Today.AddYears(-18);
+            DateTime person = new DateTime(år, månad, dag);
+
+            if (person < årsedan)
+            {
+                myndig = true;
+            }
+
+            Console.WriteLine("Din myndighetsstatus är " + myndig);
+
+        }
+    }
+}
